a000820f6866a6d3ca728feec50e565e82c9428c
alberto <albertozanola1996@gmail.com>
2021-09-07 16:56:13 +0200

    inserimento ingredienti js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@
 const cupon = ['1234aaddff', '223344ggttyy', '6789poiu'];
 const prezzoIniziale = 5;
 document.getElementById("bottone").addEventListener("click", function() {
-    const prova = document.getElementById("cheese").value;
-    console.log(prova);
+    let add1 = document.getElementById("cheese").value;
+    let add2 = document.getElementById("egg").value;
+    let add3 = document.getElementById("mustard").value;
+    let add4 = document.getElementById("tomato").value;
+    let add5 = document.getElementById("lettuce").value;
+    let add6 = document.getElementById("ketchup").value;
+    console.log(add1, add2, add3, add4, add5, add6);
     document.getElementById("prezzoPanino").innerHTML = '$' + ' ' + prezzoIniziale;
 });
 
