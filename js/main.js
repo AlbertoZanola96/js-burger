7387a0019d7fd6bcd613003621cb0aa6828fa474
alberto <albertozanola1996@gmail.com>
2021-09-07 16:31:18 +0200

    inizio js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
+const cupon = ['1234aaddff', '223344ggttyy', '6789poiu'];
+
+document.getElementById("bottone").addEventListener("click", function() {
+    document.getElementById("prezzoPanino").innerHTML = '$ 5';
+});
+
