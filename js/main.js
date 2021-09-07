ff10f6c67afa94ae0688eac79b542cf8beb2d2da
alberto <albertozanola1996@gmail.com>
2021-09-07 16:47:15 +0200

    cheese

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
 const cupon = ['1234aaddff', '223344ggttyy', '6789poiu'];
-
+const prezzoIniziale = 5;
 document.getElementById("bottone").addEventListener("click", function() {
-    document.getElementById("prezzoPanino").innerHTML = '$ 5';
+    const prova = document.getElementById("cheese").value;
+    console.log(prova);
+    document.getElementById("prezzoPanino").innerHTML = '$' + ' ' + prezzoIniziale;
 });
 
